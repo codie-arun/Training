4f962cea9720dda3ea0418fb6da437d15f4f1bfb
codie-arun <42159657+codie-arun@users.noreply.github.com>
2021-04-13 00:51:00 +0530

    Delete src directory

--- src/aspectj/CustomerBo.java
+++
@@ -1,9 +0,0 @@
-package aspectj;
-
-
-public interface CustomerBo {
-	void addCustomer();
-	String addCustomerReturnValue();
-	void addCustomerThrowException()throws Exception;
-	void addCustomerAround(String name);
-}
